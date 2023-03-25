28605945b5978912734d8a95af2d953106dfa208
sakshi lunia <luniasakshi@hotmail.com>
2023-03-26 00:34:17 +0530

    Add project files.

---
+++ SaaSProductMasterTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SaaSProductMasterTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SaaSProductMasterTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b04705ef-0b5b-4569-9fed-417d94899a93")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SaaSProductMasterTest/UnitTestCases.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SaaSProductsImport.Services;
+using SaaSProductsImport.Services.Products;
+using System.IO;
+
+namespace SaaSProductMasterTest
+{
+    [TestClass]
+    public class UnitTestCases
+    {
+        private CapterraService capterraService;
+        private FileFormatter formatterInstance;
+        public UnitTestCases()
+        {
+            capterraService = new CapterraService();
+            formatterInstance = new FileFormatter();
+        }
+        [TestMethod]
+        public void InCorrectFilePath()
+        {
+            var filePath = "someInvalidPath";
+            var exceptionMessage = "File path or name is invalid";
+
+            var exception = Assert.ThrowsException<IOException>(() => capterraService.ProcessFile(filePath));
+
+            Assert.AreEqual(exceptionMessage, exception.Message);
+        }
+        [TestMethod]
+        public void InCorrectFileExtension()
+        {
+            var fileName = "randomFile.jhst";
+            var exceptionMessage = "File path or name is invalid";
+
+            var exception = Assert.ThrowsException<IOException>(() => formatterInstance.GetFileFormatterInstance(fileName));
+
+            Assert.AreEqual(exceptionMessage, exception.Message);
+        }
+    }
+}
